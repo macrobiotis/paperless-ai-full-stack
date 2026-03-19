8dfab8c9c6291d8fb4219926fa7090554b16dfe9
Meik Oliver Kelbassa <info@macrobiotis.de>
2026-03-19 18:22:21 +0100

    Aktualisiere Docker-Compose und Setup-Skript für Ollama: Füge RAG-Embeddings und neue Modelle hinzu

--- setup-ollama.sh
+++ setup-ollama.sh
@@ -4,4 +4,14 @@ ollama serve &
 sleep 10
 ollama pull llama3.2
 ollama pull qwen2.5:7b
+
+ollama pull nomic-embed-text
+ollama pull all-minilm:l6-v2
+
+ollama pull gemma3:4b-it-qat            
+ollama pull lukasmalkmus/llama3-sauerkraut:latest      
+ollama pull starcoder2:3b      
+
+
+
 tail -f /dev/null
